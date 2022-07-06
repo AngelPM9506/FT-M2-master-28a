c2a304a39209838406c2039320508f9c2a1f0af9
AngelPM9506 <parra_mondragon@live.com>
2022-07-06 13:35:12 -0500

    Tercera clase completa con los  extras

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -12,21 +12,21 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
       resultSet.push(startEl);
       return resultSet;
     }
-  }else{
+  } else {
     if (matchFunc(startEl)) {
       resultSet.push(startEl);
     }
     startEl.childNodes.forEach(child => {
       let elements = traverseDomAndCollectElements(matchFunc, child);
       if (elements.length > 0) {
-        elements.forEach(element =>{
+        elements.forEach(element => {
           resultSet.push(element);
         });
       }
     });
   }
-    //console.log(resultSet);
-    return resultSet;
+  //console.log(resultSet.length);
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -38,10 +38,30 @@ var selectorTypeMatcher = function (selector) {
   /**primero reconocer si empieza con # o . 
    * slector es un string...
   */
+  //console.log(selector);
+  selector.trim();
   let tagclass = selector.split('.');
-  let parentMorLes = selector.split('>')
-  let parentSpace = selector.split(' ');
+  let parents = false;
+  if (selector.includes('>')) {
+    let tags = selector.split('>');
+    var parentMorLes = [];
+    tags.map(tagNamed => {
+      parentMorLes.push(tagNamed.trim().toUpperCase());
+    });
+    parents = true;
+  }
+  if (selector.includes(' ')) {
+    let tags = selector.split(' ');
+    if (tags.length === 2) {
+      var parentSpace = [];
+      tags.map(tagNamed => {
+        parentSpace.push(tagNamed.trim().toUpperCase());
+      });
+      parents = true;
+    }
+  }
   if (selector[0] === '#') {
+    //console.log(selector + ' -> #');
     return 'id';
   }
   if (selector[0] === '.') {
@@ -50,11 +70,16 @@ var selectorTypeMatcher = function (selector) {
   if (tagclass.length === 2) {
     return 'tag.class';
   }
-  if (tagclass.length === 1) {
+  if (tagclass.length === 1 && !parents) {
     return 'tag';
   }
-  if (parentMorLes.length === 2) {
-    
+  if (parentMorLes !== undefined && parentMorLes.length === 2) {
+    parentMorLes.unshift('>');
+    return parentMorLes;
+  }
+  if (parentSpace !== undefined && parentSpace.length === 2) {
+    parentSpace.unshift('&');
+    return parentSpace;
   }
 };
 
@@ -65,6 +90,7 @@ var selectorTypeMatcher = function (selector) {
 
 var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
+  //console.log(selectorType[0] === '>');
   var matchFunction;
   if (selectorType === "id") {
     matchFunction = etiqueta => {
@@ -95,21 +121,38 @@ var matchFunctionMaker = function (selector) {
       let tag = selector.split('.')[0].toUpperCase();
       let clas = selector.split('.')[1];
       if (etiqueta.tagName === tag) {
-        etiqueta.classList.forEach(tagClass =>{
+        etiqueta.classList.forEach(tagClass => {
           if (tagClass === clas) {
             tagClassEqual = true;
           }
         });
       }
       return tagClassEqual;
-     };
+    };
   } else if (selectorType === "tag") {
     matchFunction = etiqueta => {
       if (etiqueta.tagName === selector.toUpperCase()) {
         return true
       }
       return false;
-     };
+    };
+  } else if (selectorType[0] === '>') {
+    matchFunction = etiqueta => {
+      if (etiqueta.parentNode.tagName === selectorType[1] && etiqueta.tagName === selectorType[2]) {
+        return true;
+      }
+      return false;
+    }
+  } else if (selectorType[0] === '&') {
+    matchFunction = etiqueta => {
+      if (selectorType[1] === 'BODY' && etiqueta.tagName === selectorType[2]) {
+        return true;
+      }
+      if (etiqueta.parentNode.tagName === selectorType[1] && etiqueta.tagName === selectorType[2]) {
+        return true;
+      }
+      return false;
+    }
   }
   return matchFunction;
 };
