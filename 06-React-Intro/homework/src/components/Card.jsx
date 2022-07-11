020b48602c416040cf951c74b27883159a7344b4
AngelPM9506 <parra_mondragon@live.com>
2022-07-11 09:32:08 -0500

    Intro a react Terminado falta agregar estilos

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Card(props) {
-  // acá va tu código
-  return <div>Card Component</div>
-};
+class Card extends Component {
+  fToCl(f) {
+    let c = (f - 32) / 1.8;
+    return c + '°C';
+  }
+  kToCl(k) {
+    let c = k - 273.15;
+    return c + '°C';
+  }
+  urlImg(imgProps) {
+    return `http://openweathermap.org/img/wn/${imgProps}@2x.png`
+  }
+  render() {
+    return (
+      <div className='card'>
+        <button className='closeCard' onClick={this.props.onClose}>X</button>
+        <h3>{this.props.name}</h3>
+        <div className='datos'>
+          <div className='max'>
+            <p>max</p>
+            <p>{this.kToCl(this.props.max)}</p>
+          </div>
+          <div className='min'>
+            <p>min</p>
+            <p>{this.kToCl(this.props.min)}</p>
+          </div>
+          <picture>
+            <img src={this.urlImg(this.props.img)} alt="Clima" />
+          </picture>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
+// export default function Card(props) {
+//   // acá va tu código
+//   // <div>Card Component</div>
+// };
--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
-
-export default function Cards(props) {
-  // acá va tu código
-  // tip, podés usar un map
-  return <div>Cards Component</div>
-};
+import { Component } from 'react';
+import Card from './Card';
+class Cards extends Component {
+  render() {
+    return (
+      <div className='cards'>
+        {this.props.cities.map((city, i) => {
+          return (
+            <Card
+              key={i}
+              max={city.main.temp_max}
+              min={city.main.temp_min}
+              name={city.name}
+              img={city.weather[0].icon}
+              onClose={() => alert(city.name)}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+export default Cards;
+// export default function Cards(props) {
+//   // acá va tu código
+//   // tip, podés usar un map
+//   return <div>Cards Component</div>
+// };
