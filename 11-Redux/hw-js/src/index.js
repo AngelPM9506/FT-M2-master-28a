ac8a3d0cd446e62cc2aa678f90113edb8bc7dad9
AngelPM9506 <parra_mondragon@live.com>
2022-07-18 11:28:54 -0500

    Onceava clase terminada con los extras y estilos ambas HW mejoras

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -6,7 +6,6 @@ const { incremento, decremento } = require('./actions');
 var store = createStore(contador);
 // Obtenemos el elemento con el id `valor`.
 var valor = document.querySelector('#valor');
-
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
@@ -16,16 +15,10 @@ function renderContador() {
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
   valor.textContent = valContador;
 }
-
 // Ejecutamos la funcion 'renderContador':
-
 renderContador();
-
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
-
-const subscribe = store.subscribe(renderContador);
-subscribe();
-
+store.subscribe(renderContador);
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
 /**seleccion de botones */
@@ -34,27 +27,16 @@ const decrementoBtn = document.querySelector('#decremento');
 const incrementoImparBtn = document.querySelector('#incrementoImpar');
 const incrementoAsyncBtn = document.querySelector('#incrementoAsync');
 
-incrementoBtn.addEventListener('click', () => {
-  let actionIncrement = incremento();
-  store.dispatch(actionIncrement);
-  renderContador();
-});
-decrementoBtn.addEventListener('click', () => {
-  let actionDrecrement = decremento();
-  store.dispatch(actionDrecrement);
-  renderContador();
-});
+incrementoBtn.addEventListener('click', () => store.dispatch(incremento()));
+
+decrementoBtn.addEventListener('click', () => store.dispatch(decremento()));
+
 incrementoImparBtn.addEventListener('click', () => {
   if (store.getState().contador % 2 === 0) {
-    let actionIncrement = incremento();
-    store.dispatch(actionIncrement);
-    renderContador();
+    store.dispatch(incremento());
   }
 });
+
 incrementoAsyncBtn.addEventListener('click', () => {
-  setTimeout(() => {
-    let actionIncrement = incremento();
-    store.dispatch(actionIncrement);
-    renderContador();
-  }, 1000);
+  setTimeout(() => { store.dispatch(incremento()); }, 1000);
 });
